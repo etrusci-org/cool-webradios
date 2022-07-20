e0f67ca80c7a2f834eefa02e68009b4ca3fe45ed
arT2 <60965610+etrusci-org@users.noreply.github.com>
2022-07-20 12:17:03 +0200

    Renamed stations.json to webradios.json.

--- src/bake.py
+++ src/bake.py
@@ -4,7 +4,7 @@ import os
 import json
 
 
-srcFile = os.path.join('src', 'stations.json')
+srcFile = os.path.join('src', 'webradios.json')
 outFileM3U = 'cool-webradios.m3u'
 outFileHTML = 'cool-webradios.html'
 
