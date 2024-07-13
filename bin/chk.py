bd77e6db9aa090efcc1473055067a8ca93ef7174
arT2 <60965610+etrusci-org@users.noreply.github.com>
2024-07-13 15:51:57 +0200

    chk

---
+++ bin/chk.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import datetime
+import json
+import pathlib
+import time
+
+import vlc # python3-vlc
+
+
+
+
+def check_webradios(src_file: pathlib.Path, log_file: pathlib.Path, playtime: int):
+    if not SRC_FILE.is_file():
+        print(f'SRC_FILE not found: {SRC_FILE}')
+        exit(1)
+
+    source: list[dict[str, dict[str, str] | list[dict[str, str]]]] = json.loads(src_file.read_bytes())
+
+    instance: vlc.Instance = vlc.Instance()
+    player: vlc.MediaPlayer = instance.media_player_new()
+    player.audio_set_mute(True) # does not always work
+    player.audio_set_volume(0) # does not always work
+
+    errors: int = 0
+    log: list[str] = []
+
+    print('checking station channels ...')
+
+    try:
+        for webradio in source:
+            for channel in webradio['channel']:
+                print(f"{webradio['station']['name']} - {channel['name']}")
+                media: vlc.Media = instance.media_new(channel['url'])
+                player.set_media(media)
+                player.play()
+                time.sleep(playtime)
+                state: str = str(player.get_state())
+                player.stop()
+                if state.lower() != 'state.playing':
+                    log.append(f"{datetime.datetime.now()} BOO: {webradio['station']['name']} - {channel['name']} - {channel['url']}")
+                    errors += 1
+                else:
+                    log.append(f"{datetime.datetime.now()} OK: {webradio['station']['name']} - {channel['name']} - {channel['url']}")
+                time.sleep(2)
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        player.stop()
+        log_file.write_text('\n'.join(log))
+
+    if errors == 0:
+        print('everything seems ok')
+    else:
+        print(f'errors occurred, see {log_file}')
+
+    # TODO: push_notification
+
+
+
+
+if __name__ == '__main__':
+    SRC_FILE: pathlib.Path = (pathlib.Path(__file__).parents[1] / 'src' / 'webradios.json').resolve()
+    LOG_FILE: pathlib.Path = (pathlib.Path(__file__).parents[1] / 'log' / 'chk.log').resolve()
+    PLAYTIME: int = 5
+
+    check_webradios(src_file=SRC_FILE, log_file=LOG_FILE, playtime=PLAYTIME)
